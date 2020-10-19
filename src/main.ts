3f4871e9105221a12c6099f06f5cfc888a69a4d5
Alexey Antipov <antipov.alexei@gmail.com>
2020-10-19 08:42:14 +0200

    Install apollo

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  apolloProvider: createProvider(),
+  render: h => h(App)
 }).$mount('#app')
