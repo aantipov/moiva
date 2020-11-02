5992e8a8f765ec181364dce13b92c1f11d472d5a
Alexey Antipov <antipov.alexei@gmail.com>
2020-11-02 20:32:08 +0100

    install chartjs and create first chart

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
+    <h1>Vue vs React</h1>
+    <div style="width: 400px; height: 400px">
+      <canvas id="myChart" width="400" height="400"></canvas>
+    </div>
     <div v-if="$apollo.loading">Loading...</div>
     <div v-else>
-      <div>
-        Vue: {{ repos.vue.description }} 
-      </div>
-      <div>
-        React: {{ repos.react.description }}
-      </div> 
     </div>
   </div>
 </template>
@@ -15,9 +13,10 @@
 <script lang="ts">
 import Vue from 'vue';
 import gql from 'graphql-tag';
+import Chart from 'chart.js';
 
 export default Vue.extend({
-  name: 'Main',
+  name: 'MainCo',
   apollo: {
     repos: {
       update: data => data,
@@ -50,6 +49,45 @@ export default Vue.extend({
         }
       `,
     }  
+  },
+  updated() {
+    var ctx = document.getElementById('myChart');
+    if (this.$apollo.loading) {
+     return; 
+    }
+    console.log('updatee')
+    const {vue, react} = this.repos;
+    new Chart(ctx, {
+      type: 'bar',
+      data: {
+          labels: ['Vue', 'React'],
+          datasets: [
+          {
+              label: '# of open issues',
+              data: [vue.openIssues.totalCount, react.openIssues.totalCount],
+              backgroundColor: 'rgba(54, 162, 235, 0.2)',
+              borderColor: 'rgba(54, 162, 235, 1)',
+              borderWidth: 1
+          },
+          {
+              label: '# of closed issues',
+              data: [vue.closedIssues.totalCount, react.closedIssues.totalCount],
+              backgroundColor: 'rgba(255, 99, 132, 0.2)',
+              borderColor: 'rgba(255, 99, 132, 1)',
+              borderWidth: 1
+          },
+        ]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+    });
   }
 });
 </script>
