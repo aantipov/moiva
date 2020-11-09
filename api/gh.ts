d67caa7e8ee47ac4a5f10f3e5cc641097db607ae
Alexey Antipov <antipov.alexei@gmail.com>
2020-11-09 20:17:38 +0100

    convert gh call to GET to leverage caching

--- api/gh.ts
+++ api/gh.ts
@@ -5,17 +5,65 @@ export default async (req: NowRequest, res: NowResponse) => {
   const skey = process.env.GITHUB_API_KEY;
   const url = 'https://api.github.com/graphql';
 
-  const resp = await axios({
-    method: 'POST',
-    url,
-    data: req.body,
-    headers: {
-      Authorization: `Bearer ${skey}`,
-      accept: req.headers.accept,
-      'content-type': req.headers['content-type'],
-    },
-  });
-
-  res.setHeader('Cache-Control', 'max-age=0, s-maxage=43200');
-  res.status(resp.status).json(resp.data);
+  axios
+    .post(
+      url,
+      {
+        operationName: null,
+        variables: {},
+        query: `
+        {
+          vue: repository(name: "vue", owner: "vuejs") {
+            description
+            stars: stargazerCount
+            createdAt
+            openPRs: pullRequests(states: [OPEN]) {
+              totalCount
+            }
+            mergedPRs: pullRequests(states: [MERGED]) {
+              totalCount
+            }
+            closedPRs: pullRequests(states: [CLOSED]) {
+              totalCount
+            }
+            openIssues: issues(filterBy: { states: [OPEN] }) {
+              totalCount
+            }
+            closedIssues: issues(filterBy: { states: [CLOSED] }) {
+              totalCount
+            }
+          }
+          react: repository(name: "react", owner: "facebook") {
+            description
+            stars: stargazerCount
+            createdAt
+            openPRs: pullRequests(states: [OPEN]) {
+              totalCount
+            }
+            mergedPRs: pullRequests(states: [MERGED]) {
+              totalCount
+            }
+            closedPRs: pullRequests(states: [CLOSED]) {
+              totalCount
+            }
+            openIssues: issues(filterBy: { states: [OPEN] }) {
+              totalCount
+            }
+            closedIssues: issues(filterBy: { states: [CLOSED] }) {
+              totalCount
+            }
+          }
+        }
+      `,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${skey}`,
+        },
+      }
+    )
+    .then((resp) => {
+      res.setHeader('Cache-Control', 'max-age=0, s-maxage=43200');
+      res.status(resp.status).json(resp.data);
+    });
 };
