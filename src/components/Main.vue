227a46fda0c6c43d163ce7d8a7e661059da8d93f
Alexey Antipov <antipov.alexei@gmail.com>
2020-11-13 22:45:11 +0100

    add thoughtworks chart (#7)

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,6 +2,7 @@
   <div>
     <Github />
     <Npm />
+    <TechRadar style="margin-top: 24px" />
   </div>
 </template>
 
@@ -9,12 +10,14 @@
 import Vue from 'vue';
 import Npm from './Npm.vue';
 import Github from './Github.vue';
+import TechRadar from './TechRadar.vue';
 
 export default Vue.extend({
   name: 'Main',
   components: {
     Github,
     Npm,
+    TechRadar,
   },
 });
 </script>
