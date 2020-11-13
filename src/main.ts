5b9fa73ed12d2aafef9a50a541ea8c375ea53a26
Alexey Antipov <antipov.alexei@gmail.com>
2020-11-13 23:16:45 +0100

    remove apollo and graphql (#8)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { createProvider } from './vue-apollo';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  apolloProvider: createProvider(),
   render: (h) => h(App),
 }).$mount('#app');
