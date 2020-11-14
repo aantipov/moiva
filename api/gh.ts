da5ddd0eea07479ac4754e79da0f1d17d949b4f6
Alexey Antipov <antipov.alexei@gmail.com>
2020-11-14 18:59:14 +0100

    refactor Github api (#10)

--- api/gh.ts
+++ api/gh.ts
@@ -1,9 +1,11 @@
 import { NowRequest, NowResponse } from '@vercel/node';
 import axios from 'axios';
+import config from '../apps-config'
 
 export default async (req: NowRequest, res: NowResponse) => {
   const skey = process.env.GITHUB_API_KEY;
   const url = 'https://api.github.com/graphql';
+  const app = config.find(appConfig => appConfig.name === req.query.app).github
 
   axios
     .post(
@@ -13,27 +15,7 @@ export default async (req: NowRequest, res: NowResponse) => {
         variables: {},
         query: `
         {
-          vue: repository(name: "vue", owner: "vuejs") {
-            description
-            stars: stargazerCount
-            createdAt
-            openPRs: pullRequests(states: [OPEN]) {
-              totalCount
-            }
-            mergedPRs: pullRequests(states: [MERGED]) {
-              totalCount
-            }
-            closedPRs: pullRequests(states: [CLOSED]) {
-              totalCount
-            }
-            openIssues: issues(filterBy: { states: [OPEN] }) {
-              totalCount
-            }
-            closedIssues: issues(filterBy: { states: [CLOSED] }) {
-              totalCount
-            }
-          }
-          react: repository(name: "react", owner: "facebook") {
+          repository(name: "${app.name}", owner: "${app.owner}") {
             description
             stars: stargazerCount
             createdAt
@@ -64,6 +46,6 @@ export default async (req: NowRequest, res: NowResponse) => {
     )
     .then((resp) => {
       res.setHeader('Cache-Control', 'max-age=0, s-maxage=43200');
-      res.status(200).json(resp.data);
+      res.status(200).json(resp.data.data.repository);
     });
 };
---
+++ apps-config.ts
@@ -0,0 +1,11 @@
+export default [
+  {
+    name: 'vue',
+    github: {name: 'vue', owner: 'vuejs'},
+  },
+  {
+    name: 'react',
+    github: {name: 'react', owner: 'facebook'},
+  }
+]
+    
