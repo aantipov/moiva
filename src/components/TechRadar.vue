da61266ce0451ec66e87ee90b720c354fe4c7fbf
Alexey Antipov <antipov.alexei@gmail.com>
2020-11-16 10:06:05 +0100

    fix tech radar (#20)

--- src/components/TechRadar.vue
+++ src/components/TechRadar.vue
@@ -10,18 +10,23 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import Chart from 'chart.js';
+import Chart, { ChartConfiguration, ChartData } from 'chart.js';
 // @ts-ignore
 import { appsConfigsMap, TRADAR_LEVELS } from '../../apps-config';
 
 export default Vue.extend({
   name: 'TechRadar',
-
   props: {
     apps: {
       type: Array as () => string[],
       required: true,
     },
+  },
+
+  data() {
+    return {
+      chart: (null as unknown) as Chart,
+    };
   },
 
   computed: {
@@ -30,6 +35,22 @@ export default Vue.extend({
         .map((app) => Object.keys(appsConfigsMap[app].tradar.data))
         .flat();
       return [...new Set(dates)].sort();
+    },
+    chartDatasets(): any {
+      return this.apps.map((app) => ({
+        label: app,
+        fill: false,
+        data: this.uniqDates.map(
+          (date) => appsConfigsMap[app].tradar.data[date]
+        ),
+        backgroundColor: appsConfigsMap[app].color,
+        borderColor: appsConfigsMap[app].color,
+        spanGaps: true,
+        borderWidth: 2,
+        lineTension: 0,
+        pointStyle: 'triangle',
+        radius: 8,
+      }));
     },
   },
 
@@ -40,36 +61,32 @@ export default Vue.extend({
   },
 
   mounted(): void {
-    this.updateChart();
+    this.initChart();
   },
 
   methods: {
     updateChart(): void {
+      this.chart.data = {
+        labels: this.uniqDates,
+        xLabels: this.uniqDates,
+        yLabels: TRADAR_LEVELS,
+        datasets: this.chartDatasets,
+      } as ChartData;
+
+      this.chart.update();
+    },
+
+    initChart(): void {
       const ctx = document.getElementById('techRadar') as HTMLCanvasElement;
 
-      new Chart(ctx, {
+      this.chart = new Chart(ctx, {
         type: 'line',
 
         data: {
           labels: this.uniqDates,
-          // @ts-ignore
           xLabels: this.uniqDates,
           yLabels: TRADAR_LEVELS,
-          // @ts-ignore
-          datasets: this.apps.map((app) => ({
-            label: app,
-            fill: false,
-            data: this.uniqDates.map(
-              (date) => appsConfigsMap[app].tradar.data[date]
-            ),
-            backgroundColor: appsConfigsMap[app].color,
-            borderColor: appsConfigsMap[app].color,
-            spanGaps: true,
-            borderWidth: 2,
-            lineTension: 0,
-            pointStyle: 'triangle',
-            radius: 8,
-          })),
+          datasets: this.chartDatasets,
         },
 
         options: {
@@ -81,7 +98,7 @@ export default Vue.extend({
             yAxes: [{ type: 'category', ticks: { reverse: true } }],
           },
         },
-      });
+      } as ChartConfiguration);
     },
   },
 });
