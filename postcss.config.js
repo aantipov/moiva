f59ae60f1e1236fa97bcf626f6479e65aa05391f
Alexey Antipov <antipov.alexei@gmail.com>
2020-11-21 13:49:14 +0100

    ddd

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    // 'vue-cli-plugin-tailwind/purgecss': {
-    //   enabled: false,
-    //   purge: {
-    //     enabled: false,
-    //   },
-    // },
+    'vue-cli-plugin-tailwind/purgecss': {},
     autoprefixer: {},
   },
 };
