aa95fb6ba5e814b9b8d3401ada1758b2677f96bd
Alexey Antipov <antipov.alexei@gmail.com>
2020-11-25 08:33:29 +0100

    improve-logging

--- api/utils.ts
+++ api/utils.ts
@@ -2,22 +2,25 @@ import * as faunadb from 'faunadb';
 
 export function logRequest(type: 'npm' | 'github', urlQuery: unknown): void {
   const skey = process.env.FAUNA_DB;
-  const env = process.env.NODE_ENV;
+  const { VERCEL_ENV, VERCEL_REGION } = process.env;
 
-  if (env === 'development') {
+  if (VERCEL_ENV === 'development') {
     return;
   }
 
   const serverClient = new faunadb.Client({ secret: skey });
   const q = faunadb.query;
+  const now = new Date();
+  const ts = `${now.getFullYear()}-${now.getMonth()}-${now.getDay()}--${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
 
   serverClient
     .query(
       q.Create(q.Collection('api_calls_logs'), {
         data: {
           type,
-          env: process.env.NODE_ENV,
-          ts: new Date().toISOString(),
+          env: VERCEL_ENV,
+          ts,
+          region: VERCEL_REGION,
           urlQuery: JSON.stringify(urlQuery),
         },
       })
