f219d62fec04236bb1265979a8326a80754d59f1
Alexey Antipov <antipov.alexei@gmail.com>
2020-11-28 08:35:18 +0100

    remove comments

--- src/components/GithubReleases.vue
+++ src/components/GithubReleases.vue
@@ -5,9 +5,7 @@
 <script lang="ts">
 import Vue from 'vue';
 import Chart from 'chart.js';
-// @ts-ignore
 import { RepoT } from '../apis';
-// @ts-ignore
 import { appsConfigsMap } from '../../apps-config';
 
 const years = [2017, 2018, 2019, 2020];
@@ -50,30 +48,12 @@ export default Vue.extend({
           radius: 6,
           pointHoverRadius: 10,
         })),
-
-        // return this.apps.map((app) => ({
-        //   label: app,
-        //   fill: false,
-        //   data: this.uniqDates.map(
-        //     (date) => appsConfigsMap[app].tradar.data[date]
-        //   ),
-        //   backgroundColor: appsConfigsMap[app].color,
-        //   borderColor: appsConfigsMap[app].color,
-        //   spanGaps: true,
-        //   borderWidth: 3,
-        //   lineTension: 0,
-        //   radius: 6,
-        //   pointHoverRadius: 10,
-        // }));
       },
 
       options: {
         title: {
           display: true,
           text: 'Release frequency, count/year',
-        },
-        scales: {
-          // yAxes: [{ type: 'category', ticks: { reverse: true } }],
         },
       },
     });
