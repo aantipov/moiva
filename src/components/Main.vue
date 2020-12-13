5882234e974c5bf44890ed8e25fa429a0d8c3ebe
Alexey Antipov <antipov.alexei@gmail.com>
2020-12-13 17:08:51 +0100

    Hide input before default libs loading is finished

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="w-full mx-auto xl:w-2/3">
-      <Autosuggest @select="select" />
+      <Autosuggest v-show="!loadingDefaultLibs" @select="select" />
 
       <!--  Selected libs  -->
       <div>
