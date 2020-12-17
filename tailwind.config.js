8cf316c8db3882d3ca8d183fc50450317fffb1f6
Alexey Antipov <antipov.alexei@gmail.com>
2020-12-17 07:58:52 +0100

    fix tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  future: {
-    // removeDeprecatedGapUtilities: true,
-    // purgeLayersByDefault: true,
-  },
   purge: {
     content: ['./public/**/*.html', './src/**/*.vue'],
   },
