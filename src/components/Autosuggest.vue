909900492a478070084c6c9a4c4b29678ed35d95
Alexey Antipov <antipov.alexei@gmail.com>
2020-12-17 08:31:13 +0100

    update tailwind to 2.0 (#54)

--- src/components/Autosuggest.vue
+++ src/components/Autosuggest.vue
@@ -5,7 +5,8 @@
       type="text"
       placeholder="Add npm packages to comparison"
       autofocus
-      class="relative w-full px-3 py-3 pr-10 text-lg text-gray-700 placeholder-gray-500 bg-white border border-gray-400 rounded outline-none focus:outline-none focus:border-3 focus:bg-gray-300"
+      autocomplete="off"
+      class="relative w-full px-3 py-3 pr-10 text-xl font-light text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded outline-none focus:border-gray-400 focus:outline-none focus:border-3 focus:bg-gray-100"
     />
     <span
       class="absolute right-0 z-10 items-center justify-center w-8 h-full py-3 pr-3 text-base font-normal leading-snug text-center text-gray-400 bg-transparent rounded"
@@ -70,7 +71,7 @@ export default Vue.extend({
 
 <style lang="postcss">
 .ac {
-  @apply rounded rounded-t-none border border-t-0 border-orange-300;
+  @apply rounded rounded-t-none border border-t-0 border-gray-300;
 }
 .ac > .ac-option {
   @apply h-12 px-5 border-b flex flex-col justify-center;
@@ -82,7 +83,7 @@ export default Vue.extend({
 }
 .ac > .ac-option.selected,
 .ac > .ac-option:hover:not(.group) {
-  @apply bg-orange-300;
+  @apply bg-gray-100;
 }
 .ac-option-title {
   @apply h-5 overflow-hidden text-gray-800 font-normal text-base;
