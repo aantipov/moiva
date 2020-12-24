9005be928c85775c5910ee87df9a53a4ba48654e
Alexey Antipov <antipov.alexei@gmail.com>
2020-12-24 09:22:07 +0100

    ddd

--- api/bphobia.ts
+++ api/bphobia.ts
@@ -23,7 +23,7 @@ export default (req: NowRequest, res: NowResponse): void => {
       res.status(200).json({ gzip: resp.data.gzip, raw: resp.data.size });
     })
     .catch((e) => {
-      console.error('ERROR', e.response);
+      // console.error('ERROR', e.response);
       res.status(500).json({ error: 'Something went wrong!!!' });
       // try {
       //   reportError(e);
