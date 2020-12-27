f8b209daf447f69e6a9f1c74ba0a13698d74b2db
Alexey Antipov <antipov.alexei@gmail.com>
2020-12-27 09:35:54 +0100

    migrate Main and Dependencies components to composition api

--- src/components/Dependencies.vue
+++ src/components/Dependencies.vue
@@ -9,8 +9,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Chart from 'chart.js';
+import { defineComponent, onMounted, watch, toRefs, computed } from 'vue';
+import Chart, { ChartDataSets } from 'chart.js';
 import { LibraryT } from '../apis';
 import { COLOR_GRAY } from '../colors';
 
@@ -24,45 +24,64 @@ export default defineComponent({
     },
   },
 
-  mounted(): void {
-    const ctx = document.getElementById('dependencies') as HTMLCanvasElement;
-    const { libs } = this;
-
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: libs.map((lib) => lib.name),
-        datasets: [
+  setup(props) {
+    const { libs } = toRefs(props);
+    const labels = computed<string[]>(() => libs.value.map((lib) => lib.name));
+    const datasets = computed<ChartDataSets[]>(
+      () =>
+        [
           {
             label: 'dependencies',
-            data: libs.map((lib) => lib.dependencies.length),
+            data: libs.value.map((lib) => lib.dependencies.length),
             backgroundColor: COLOR_GRAY,
             borderWidth: 1,
           },
-        ],
-      },
+        ] as ChartDataSets[]
+    );
+    let mychart: Chart | undefined;
 
-      options: {
-        title: {
-          display: false,
-          text: 'Dependencies',
+    function initChart(): void {
+      const ctx = document.getElementById('dependencies') as HTMLCanvasElement;
+
+      mychart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: labels.value,
+          datasets: datasets.value,
         },
-        legend: {
-          display: false,
-        },
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-                // @ts-ignore
-                precision: 0,
+
+        options: {
+          title: {
+            display: false,
+            text: 'Dependencies',
+          },
+          legend: {
+            display: false,
+          },
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  // @ts-ignore
+                  precision: 0,
+                },
               },
-            },
-          ],
+            ],
+          },
         },
-      },
+      });
+    }
+
+    onMounted(initChart);
+
+    watch(libs, () => {
+      (mychart as Chart).data.labels = labels.value;
+      (mychart as Chart).data.datasets = datasets.value;
+      (mychart as Chart).update();
     });
+
+    return {};
   },
 });
 </script>
