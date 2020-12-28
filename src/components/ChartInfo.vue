fbd3190dc505e694eb84b131459b735fcd734244
Alexey Antipov <antipov.alexei@gmail.com>
2020-12-28 09:16:22 +0100

    provide info for Vulnerabilities chart

---
+++ src/components/ChartInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <span ref="triggerRef" class="p-1 rounded cursor-pointer hover:bg-gray-200"
+      >&#128216;</span
+    >
+    <div ref="contentRef">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue';
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light.css';
+import 'tippy.js/themes/light-border.css';
+
+export default defineComponent({
+  name: 'ChartInfo',
+  setup() {
+    const contentRef = ref(null);
+    const triggerRef = ref(null);
+
+    onMounted(() => {
+      tippy((triggerRef.value as unknown) as HTMLElement, {
+        content: (contentRef.value as unknown) as HTMLElement,
+        // trigger: 'click',
+        interactive: true,
+        allowHTML: true,
+        theme: 'moiva',
+      });
+    });
+
+    return { triggerRef, contentRef };
+  },
+});
+</script>
+
+<style lang="postcss">
+.tippy-box[data-theme~='moiva'] {
+  @apply bg-gray-50 border border-gray-200 shadow text-base;
+}
+.tippy-box[data-theme~='moiva'] > .tippy-arrow {
+  @apply hidden;
+}
+</style>
