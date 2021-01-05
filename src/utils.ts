1b1ce2a6d1fdfc390ac0b84d1d408618486f5b3e
Alexey Antipov <antipov.alexei@gmail.com>
2021-01-05 19:15:21 +0100

    cleanup

--- src/utils.ts
+++ src/utils.ts
@@ -190,11 +190,8 @@ export function getSuggestions(libsNames: string[]): string[] {
   const lastSelectedLibData = libsDataByName[libsNames[libsNames.length - 1]];
 
   if (!lastSelectedLibData || lastSelectedLibData.category === 'Other') {
-    console.log('Nothing', lastSelectedLibData, libsNames);
     return [];
   }
-
-  console.log('Oh no');
 
   return libsNamesByCategory[lastSelectedLibData.category]
     .filter((libName) => !libsNames.includes(libName))
