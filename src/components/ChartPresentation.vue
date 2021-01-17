18bf972a45e2d33e28abcd235ae4f091a3984c5f
Alexey Antipov <antipov.alexei@gmail.com>
2021-01-17 10:47:15 +0100

    create chartPresentational component and use it in LanguagesChart (#157)
    * create chartPresentational component and use it in LanguagesChart
    
    * small refactor
    
    * refactor
    
    * cleanup

---
+++ src/components/ChartPresentation.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <!-- Header -->
+    <div class="flex items-center justify-center mt-5">
+      <h2 class="my-0">{{ title }}</h2>
+
+      <m-chart-info class="ml-2"><slot /></m-chart-info>
+
+      <m-chart-info v-if="failedLibsNames.length" class="ml-2" type="WARNING">
+        <div>
+          Sorry, we couldn't fetch data for the following packages:
+          <div v-for="libName in failedLibsNames" :key="libName">
+            - {{ libName }}
+          </div>
+
+          Try reload the page or check later.
+        </div>
+      </m-chart-info>
+    </div>
+
+    <!-- Chart -->
+    <div class="relative" style="height: 350px">
+      <m-loader v-if="isLoading" />
+
+      <div v-else-if="isError || !libsNames.length" class="chart-error-new">
+        <div>
+          Sorry we couldn't load the data. <br />
+          Try reload the page or check later
+        </div>
+      </div>
+
+      <canvas v-show="!isError && libsNames.length" ref="chartEl"></canvas>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, toRefs, onMounted, watch } from 'vue';
+import Chart from 'chart.js';
+
+export default defineComponent({
+  name: 'ChartPresentation',
+
+  props: {
+    title: { type: String, required: true },
+    isLoading: { type: Boolean, required: true },
+    isError: { type: Boolean, required: true },
+    libsNames: { type: Array as () => string[], required: true },
+    failedLibsNames: { type: Array as () => string[], required: true },
+    chartConfig: {
+      type: Object as () => Chart.ChartConfiguration,
+      required: true,
+    },
+  },
+
+  setup(props) {
+    const { isLoading, chartConfig, isError } = toRefs(props);
+    const chartEl = ref<null | HTMLCanvasElement>(null);
+    let mychart: Chart | undefined;
+
+    function initChart(): void {
+      const ctx = chartEl.value as HTMLCanvasElement;
+      mychart = new Chart(ctx, chartConfig.value);
+    }
+
+    onMounted(initChart);
+
+    watch([chartConfig, isLoading, isError], () => {
+      if (!isLoading.value && !isError.value) {
+        (mychart as Chart).data.labels = chartConfig.value.data?.labels;
+        (mychart as Chart).data.datasets = chartConfig.value.data?.datasets;
+        (mychart as Chart).update();
+      }
+    });
+
+    return {
+      chartEl,
+    };
+  },
+});
+</script>
--- src/components/LanguagesChart.vue
+++ src/components/LanguagesChart.vue
@@ -1,62 +1,32 @@
 <template>
-  <div>
-    <!-- Header -->
-    <div class="flex items-center justify-center mt-5">
-      <h2 class="my-0">Languages, %</h2>
-
-      <m-chart-info class="ml-2">
-        <p>
-          This chart shows distribution of languages used in libraries'
-          repostories.
-        </p>
-        <p>
-          Calculation is based on the number of bytes of code written in each
-          language.
-        </p>
-      </m-chart-info>
-
-      <m-chart-info v-if="failedLibsNames.length" class="ml-2" type="WARNING">
-        <div>
-          Sorry, we couldn't fetch data for the following packages:
-          <div v-for="libName in failedLibsNames" :key="libName">
-            - {{ libName }}
-          </div>
-
-          Try reload the page or check later.
-        </div>
-      </m-chart-info>
-    </div>
-
-    <!-- Chart -->
-    <div class="relative" style="height: 350px">
-      <m-loader v-if="isLoading || isLoadingLibsData" />
-
-      <div
-        v-else-if="isError || !filteredLibsNames.length"
-        class="chart-error-new"
-      >
-        <div>
-          Sorry we couldn't load the data. <br />
-          Try reload the page or check later
-        </div>
-      </div>
-
-      <canvas
-        v-show="!isError && filteredLibsNames.length"
-        id="languages"
-      ></canvas>
-    </div>
-  </div>
+  <ChartPresentation
+    title="Languages, %"
+    :is-loading="isLoading || isLoadingLibsData"
+    :is-error="isError"
+    :libs-names="filteredLibsNames"
+    :failed-libs-names="failedLibsNames"
+    :chart-config="chartConfig"
+  >
+    <p>
+      This chart shows distribution of languages used in libraries' repostories.
+    </p>
+    <p>
+      Calculation is based on the number of bytes of code written in each
+      language.
+    </p>
+  </ChartPresentation>
 </template>
 
 <script lang="ts">
-import { defineComponent, toRefs, onMounted, watch, computed } from 'vue';
+import { defineComponent, toRefs, computed } from 'vue';
 import Chart, { ChartDataSets } from 'chart.js';
 import { getLangToColorMap } from '@/colors';
 import { GithubLanguagesResponseT } from '../../api/gh-languages';
+import ChartPresentation from './ChartPresentation.vue';
 
 export default defineComponent({
   name: 'LanguagesChart',
+  components: { ChartPresentation },
 
   props: {
     isLoadingLibsData: { type: Boolean, required: true },
@@ -70,13 +40,9 @@ export default defineComponent({
   },
 
   setup(props) {
-    const {
-      libsNames,
-      libsLanguages,
-      isLoadingLibsData,
-      isLoading,
-      isError,
-    } = toRefs(props);
+    const { libsNames, libsLanguages, isLoadingLibsData, isLoading } = toRefs(
+      props
+    );
 
     const filteredLibsLanguages = computed<GithubLanguagesResponseT[]>(
       () =>
@@ -189,40 +155,22 @@ export default defineComponent({
           borderWidth: 1,
         })) as ChartDataSets[]
     );
-    let mychart: Chart | undefined;
 
-    function initChart(): void {
-      const ctx = document.getElementById('languages') as HTMLCanvasElement;
-
-      mychart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: filteredLibsNames.value,
-          datasets: datasets.value,
-        },
-
-        options: {
-          scales: {
-            xAxes: [{ stacked: true }],
-            yAxes: [{ stacked: true }],
-          },
-        },
-      });
-    }
-
-    onMounted(initChart);
-
-    watch([libsNames, languagesNames, isLoading, isError], () => {
-      if (!isLoading.value && !isError.value) {
-        (mychart as Chart).data.labels = filteredLibsNames.value;
-        (mychart as Chart).data.datasets = datasets.value;
-        (mychart as Chart).update();
-      }
-    });
+    const chartConfig = computed<Chart.ChartConfiguration>(() => ({
+      type: 'bar',
+      data: {
+        labels: filteredLibsNames.value,
+        datasets: datasets.value,
+      },
+      options: {
+        scales: { xAxes: [{ stacked: true }], yAxes: [{ stacked: true }] },
+      },
+    }));
 
     return {
       failedLibsNames,
       filteredLibsNames,
+      chartConfig,
     };
   },
 });
