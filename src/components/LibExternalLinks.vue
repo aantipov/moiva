9a3092c331545831d9f362d94096b8dda82f2757
Alexey Antipov <antipov.alexei@gmail.com>
2021-01-25 23:21:28 +0100

    show github name in selected

--- src/components/LibExternalLinks.vue
+++ src/components/LibExternalLinks.vue
@@ -1,17 +1,5 @@
 <template>
   <div class="flex items-center space-x-3">
-    <a :href="npmUrl" target="_blank" class="inline-block">
-      <NpmIcon class="w-10 sm:w-12" />
-    </a>
-
-    <a :href="repoUrl" target="_blank" class="inline-block">
-      <GithubIcon class="w-5 h-5 sm:w-6 sm:h-6" />
-    </a>
-
-    <a :href="bundlephobiaUrl" target="_blank" class="inline-block">
-      <BundlephobiaIcon class="w-5 sm:w-6" />
-    </a>
-
     <a
       v-if="thoughtworksUrl"
       :href="thoughtworksUrl"
@@ -20,15 +8,16 @@
     >
       <ThoughtworksIcon class="w-5 sm:w-6" />
     </a>
-    <span v-else class="w-5 sm:w-6"></span>
+
+    <a :href="bundlephobiaUrl" target="_blank" class="inline-block">
+      <BundlephobiaIcon class="w-5 sm:w-6" />
+    </a>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, toRefs, computed } from 'vue';
-import GithubIcon from './icons/Github.vue';
 import BundlephobiaIcon from './icons/Bundlephobia.vue';
-import NpmIcon from './icons/Npm.vue';
 import ThoughtworksIcon from './icons/Thoughtworks.vue';
 import { libsToLinkMap } from '../../techradar.config';
 import { getBundlephobiaUrl } from '@/utils';
@@ -37,9 +26,7 @@ export default defineComponent({
   name: 'LibExternalLinks',
 
   components: {
-    GithubIcon,
     BundlephobiaIcon,
-    NpmIcon,
     ThoughtworksIcon,
   },
 
@@ -48,24 +35,16 @@ export default defineComponent({
       type: String,
       required: true,
     },
-    repoUrl: {
-      type: String,
-      required: true,
-    },
   },
 
   setup(props) {
     const { libName } = toRefs(props);
-    const npmUrl = computed(
-      () => `https://www.npmjs.com/package/${encodeURIComponent(libName.value)}`
-    );
     const bundlephobiaUrl = computed(() => getBundlephobiaUrl(libName.value));
     const thoughtworksUrl = computed<string | null>(
       () => libsToLinkMap[libName.value] || null
     );
 
     return {
-      npmUrl,
       bundlephobiaUrl,
       thoughtworksUrl,
     };
