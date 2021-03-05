437d6f528f302c58d110967ee36d3bb2ac0e109c
Alexey Antipov <antipov.alexei@gmail.com>
2021-03-05 08:32:52 +0100

    use new endpoint for contributors

--- src/apis.ts
+++ src/apis.ts
@@ -130,19 +130,17 @@ export function fetchRepoCommits(
 export function fetchContributors(
   repoId: string
 ): Promise<GithubContributorsResponseItemT[] | null> {
-  const [owner, name] = repoId.split('/');
-
   if (githubContributorsCache.get(repoId)) {
     return Promise.resolve(githubContributorsCache.get(repoId));
   }
 
   return axios
-    .get<GithubContributorsResponseItemT[]>(
-      `/api/gh-contributors?name=${name}&owner=${owner}`
+    .get<{ items: GithubContributorsResponseItemT[] }>(
+      `https://github-contributors.moiva.workers.dev/?repo=${repoId}`
     )
     .then(({ data }) => {
-      githubContributorsCache.set(repoId, data);
-      return data;
+      githubContributorsCache.set(repoId, data.items);
+      return data.items;
     })
     .catch((err) => {
       const errorCode =
