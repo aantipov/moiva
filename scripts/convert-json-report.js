a5cd4a1bfb5cb3275eb2da147028c524def90ec3
Alexey Antipov <antipov.alexei@gmail.com>
2021-04-10 16:32:46 +0200

    add report for frontend frameworks

---
+++ scripts/convert-json-report.js
@@ -0,0 +1,151 @@
+/* eslint-disable */
+const fs = require('fs');
+
+const content = fs.readFileSync(
+  '../reports/frameworks-2021-q1-raw.json',
+  'utf8'
+);
+const data = JSON.parse(content);
+const libs = [
+  ['facebook/react', 'react'],
+  ['vuejs/vue', 'vue'],
+  ['angular/angular', '@angular/core'],
+  ['sveltejs/svelte', 'svelte'],
+  ['emberjs/ember.js', 'ember-source'],
+  ['alpinejs/alpine', 'alpinejs'],
+  ['infernojs/inferno', 'inferno'],
+  ['preactjs/preact', 'preact'],
+  ['jorgebucaran/hyperapp', 'hyperapp'],
+  ['riot/riot', 'riot'],
+  ['angular/angular.js', 'angular'],
+  ['marionettejs/backbone.marionette', 'backbone.marionette'],
+  ['knockout/knockout', 'knockout'],
+  ['ryansolid/solid', 'solid-js'],
+  ['MithrilJS/mithril.js', 'mithril'],
+  ['aurelia/framework', 'aurelia-framework'],
+  ['hotwired/stimulus', 'stimulus'],
+  ['marko-js/marko', 'marko'],
+];
+
+const result = libs.map(([repo], i) => {
+  const repoStars = (() => {
+    const { april, totalCount, ...starsData } = data.stars[i];
+    const { items, ...starsMonthly } = data.starsMonthly[i];
+    if (starsData.repo !== repo || starsMonthly.repo !== repo) {
+      throw new Error('invalid repo', repo, starsData.repo, starsMonthly.repo);
+    }
+    // Calc +stars, +stars %, totalAmount
+    const starsPlus = items[0].stars + items[1].stars + items[2].stars;
+    const starsStartAmount = totalCount - april - starsPlus;
+    let starsPlusPercentage = (() => {
+      const raw = (100 * starsPlus) / starsStartAmount;
+      if (raw < 10) {
+        return Math.round(raw * 10) / 10;
+      }
+      return Math.round(raw);
+    })();
+
+    return {
+      repo,
+      starsPlus,
+      starsPlusPercentage,
+      starsTotal: totalCount,
+    };
+  })();
+
+  const ghData = (() => {
+    const {
+      github: { description, createdAt },
+      contributors,
+      commits,
+      ...rest
+    } = data.other[i];
+
+    if (rest.repo !== repo) {
+      throw new Error('invalid repo');
+    }
+
+    return {
+      descr: description,
+      createdAt,
+      commits,
+      contributors: contributors.contributors,
+    };
+  })();
+
+  const npmData = (() => {
+    const { npmDetails, npmReleases, npmDownloads, ...rest } = data.npm[i];
+
+    if (rest.repo !== repo) {
+      throw new Error('invalid repo');
+    }
+
+    const dwnlMonthly = Math.round(
+      npmDownloads.slice(-3).reduce((sum, a) => sum + a.downloads, 0) / 3
+    );
+    const dwnlMonthlyPrev = Math.round(
+      npmDownloads.slice(0, 3).reduce((sum, a) => sum + a.downloads, 0) / 3
+    );
+    const dwnlMonthlyIncreasePercentage = Math.round(
+      (100 * (dwnlMonthly - dwnlMonthlyPrev)) / dwnlMonthlyPrev
+    );
+
+    return {
+      license: npmDetails.license,
+      dependencies: npmDetails.dependencies.length,
+      tsSupport: (() => {
+        const { hasBuiltinTypes, hasOtherTypes } = npmDetails;
+        if (hasBuiltinTypes) {
+          return 'BUNDLED';
+        }
+        if (hasOtherTypes) {
+          return 'SEPARATE';
+        }
+        return null;
+      })(),
+      npmReleases,
+      dwnlMonthly,
+      dwnlMonthlyIncreasePercentage,
+    };
+  })();
+
+  const radarData = (() => {
+    val = data.radar[repo.toLowerCase()];
+    return { techRadar: val || null };
+  })();
+
+  const bundleSizeData = (() => {
+    const { bundleSize, ...rest } = data.bundleSize[i];
+
+    if (rest.repo !== repo) {
+      throw new Error('invalid repo');
+    }
+
+    return { bundleSize: bundleSize || null };
+  })();
+
+  const devUsage = (() => {
+    const val = data.devUsage[repo.toLowerCase()];
+    return { devUsage: val || null };
+  })();
+
+  return {
+    ...repoStars,
+    ...ghData,
+    ...npmData,
+    ...radarData,
+    ...bundleSizeData,
+    ...devUsage,
+  };
+});
+
+fs.writeFile(
+  '../reports/frameworks-2021-q1.json',
+  JSON.stringify(result),
+  (err) => {
+    if (err) return console.log(err);
+    console.log('Data generated successfully');
+  }
+);
+
+console.log(result);
---
+++ scripts/write-report.js
@@ -0,0 +1,573 @@
+/* eslint-disable */
+const fs = require('fs');
+const fetch = require('/Users/alexey/.nvm/versions/node/v14.15.0/lib/node_modules/node-fetch');
+
+const catalog = fs.readFileSync(
+  '../src/components/developer-usage/stateof-js-css-data.json',
+  'utf8'
+);
+const catalogItems = JSON.parse(catalog);
+
+const libs = [
+  ['facebook/react', 'react'],
+  ['vuejs/vue', 'vue'],
+  ['angular/angular', '@angular/core'],
+  ['sveltejs/svelte', 'svelte'],
+  ['emberjs/ember.js', 'ember-source'],
+  ['alpinejs/alpine', 'alpinejs'],
+  ['infernojs/inferno', 'inferno'],
+  ['preactjs/preact', 'preact'],
+  ['jorgebucaran/hyperapp', 'hyperapp'],
+  ['riot/riot', 'riot'],
+  ['angular/angular.js', 'angular'],
+  ['marionettejs/backbone.marionette', 'backbone.marionette'],
+  ['knockout/knockout', 'knockout'],
+  ['ryansolid/solid', 'solid-js'],
+  ['MithrilJS/mithril.js', 'mithril'],
+  ['aurelia/framework', 'aurelia-framework'],
+  ['hotwired/stimulus', 'stimulus'],
+  ['marko-js/marko', 'marko'],
+];
+
+// Promise.all(libs.map(([repo, pkg]) => fetchBundleSize(repo, pkg)))
+//   .then((data) => {
+//     console.log(JSON.stringify(data));
+//     // console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// Check repos for new names
+// Promise.all(catalogItems.map(({ repoId }) => fetchRepoRealName(repoId)))
+//   .then((data) => {
+//     console.log(
+//       data.filter((item) => item.old.toLowerCase() !== item.new.toLowerCase())
+//     );
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// Show DevUsage Data
+// (() => {
+//   const data = libs
+//     .map(([repo]) =>
+//       catalogItems.find(
+//         ({ repoId }) => repo.toLowerCase() === repoId.toLowerCase()
+//       )
+//     )
+//     .filter(Boolean)
+//     .reduce((acc, { repoId, usage }) => {
+//       const usage2020 = usage.slice(-1)[0].year === 2020 ? usage.pop() : null;
+//       acc[repoId.toLowerCase()] = usage2020.value;
+//       return acc;
+//     }, {});
+//
+//   console.log(JSON.stringify(data));
+// })();
+
+// Show Tech Radar Data
+// (() => {
+//   const techRadarLibs = getTechradarLibs();
+//   const data = libs
+//     .map(([repo]) =>
+//       techRadarLibs.find(([radarRepo]) => repo.toLowerCase() === radarRepo)
+//     )
+//     .filter(Boolean)
+//     .reduce((acc, [repo, url, level]) => {
+//       acc[repo] = { url, level };
+//       return acc;
+//     }, {});
+//   console.log(JSON.stringify(data));
+// })();
+
+async function fetchLastMonthStars(repo) {
+  // github stars new
+  const response = await fetch(`http://127.0.0.1:8787/?repo=${repo}`, {
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (!response.ok) {
+    throw response;
+  }
+
+  const { items, totalCount } = await response.json();
+
+  return { repo, march: items[0].stars, april: items[1].stars, totalCount };
+}
+
+async function fetchLastQuarterStars(repo) {
+  const response = await fetch(
+    `https://github-stars.moiva.workers.dev/?repo=${repo}`,
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+
+  if (!response.ok) {
+    throw response;
+  }
+
+  const { items } = await response.json();
+  const last = items.slice(-3);
+
+  if (
+    last[0].month !== '2021-01' ||
+    last[1].month !== '2021-02' ||
+    last[2].month !== '2021-03'
+  ) {
+    throw new Error('wrong month');
+  }
+
+  return { repo, items: last };
+}
+
+async function fetchGithubOther(repo) {
+  const [github, contributors, commits] = await Promise.all([
+    fetchIssues(repo),
+    fetchContributors(repo),
+    fetchCommits(repo),
+  ]);
+
+  return { repo, github, contributors, commits };
+}
+
+async function fetchNpmOther(repo, pkg) {
+  const [npmDetails, npmReleases, npmDownloads] = await Promise.all([
+    fetchNpmDetails(pkg),
+    fetchNpmReleases(pkg),
+    fetchNpmDownloads(pkg),
+  ]);
+
+  return { repo, pkg, npmDetails, npmReleases, npmDownloads };
+}
+
+async function fetchIssues(repo) {
+  const response = await fetch(`http://127.0.0.1:8787/?repo=${repo}`, {
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (!response.ok) {
+    throw response;
+  }
+
+  const data = await response.json();
+
+  return data;
+}
+
+async function fetchContributors(repo) {
+  const response = await fetch(
+    `https://github-contributors.moiva.workers.dev/?repo=${repo}`,
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+
+  if (!response.ok) {
+    throw response;
+  }
+
+  const { items } = await response.json();
+
+  return items.pop();
+}
+
+async function fetchCommits(repo) {
+  const response = await fetch(
+    `https://github-commits.moiva.workers.dev/?repo=${repo}`,
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+
+  if (!response.ok) {
+    throw response;
+  }
+
+  const { items } = await response.json();
+
+  return items
+    .filter(({ week }) => {
+      const date = new Date(week * 1000);
+      const month = date.getUTCMonth();
+      const year = date.getUTCFullYear();
+      return year === 2021 && month !== 3;
+    })
+    .map(({ total }) => total)
+    .reduce((sum, a) => sum + a, 0);
+}
+
+async function fetchNpmDetails(pkg) {
+  const response = await fetch(
+    `https://npm-details.moiva.workers.dev/?pkg=${pkg}`,
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+
+  if (!response.ok) {
+    throw response;
+  }
+
+  const data = await response.json();
+
+  return data;
+}
+
+async function fetchNpmReleases(pkg) {
+  const response = await fetch(
+    `https://npm-releases.moiva.workers.dev/?pkg=${pkg}`,
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+
+  if (!response.ok) {
+    throw response;
+  }
+
+  const { items } = await response.json();
+
+  const lastQuarter = items.pop();
+
+  if (lastQuarter.month !== '2021-03') {
+    throw new Error('Wrong Release Quarter');
+  }
+
+  return lastQuarter.releases;
+}
+
+async function fetchNpmDownloads(pkg) {
+  const response = await fetch(
+    `https://npm-downloads.moiva.workers.dev/?pkg=${pkg}`,
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+
+  if (!response.ok) {
+    throw response;
+  }
+
+  const { items } = await response.json();
+
+  return items.filter(({ month }) => {
+    return [
+      '2020-10',
+      '2020-11',
+      '2020-12',
+      '2021-01',
+      '2021-02',
+      '2021-03',
+    ].includes(month);
+  });
+}
+
+async function fetchBundleSize(repo, pkg) {
+  const response = await fetch(
+    `https://bundle-size.moiva.workers.dev/?pkg=${pkg}`,
+    { headers: { 'Content-Type': 'application/json' } }
+  );
+
+  if (!response.ok) {
+    if (response.status === 404) {
+      return { repo, bundleSize: null };
+    }
+    throw response;
+  }
+
+  const bundleSize = await response.json();
+
+  return { repo, bundleSize };
+}
+
+// USE REAL TOKEN
+async function fetchRepoRealName(oldRepoId) {
+  const response = await fetch(`http://api.github.com/repos/${oldRepoId}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: 'token <TOKEN>',
+    },
+  });
+  if (!response.ok) {
+    console.error('ERR', oldRepoId, response.status);
+    if (response.status === 404 || response.status === 451) {
+      return false;
+    }
+    throw response;
+  }
+
+  const { full_name } = await response.json();
+
+  return { old: oldRepoId, new: full_name };
+}
+
+function getTechradarLibs() {
+  return [
+    [
+      'vuejs/vue',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/vue-js',
+      'ADOPT',
+    ],
+    [
+      'facebook/react',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/react-js',
+      'ADOPT',
+    ],
+    [
+      'sveltejs/svelte',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/svelte',
+      'ASSESS',
+    ],
+    [
+      'angular/angular',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/angular',
+      'TRIAL',
+    ],
+    [
+      'angular/angular.js',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/angularjs',
+      'HOLD',
+    ],
+    [
+      'emberjs/ember.js',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/ember-js',
+      'ADOPT',
+    ],
+    [
+      'reduxjs/redux',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/redux',
+      'TRIAL',
+    ],
+    [
+      'facebookexperimental/recoil',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/recoil',
+      'ASSESS',
+    ],
+    [
+      'davidkpiano/xstate',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/xstate',
+      'TRIAL',
+    ],
+    [
+      'testing-library/react-testing-library',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/react-testing-library',
+      'ADOPT',
+    ],
+    [
+      'enzymejs/enzyme',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/enzyme',
+      'HOLD',
+    ],
+    [
+      'facebook/jest',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/jest',
+      'TRIAL',
+    ],
+    [
+      'timkindberg/jest-when',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/jest-when',
+      'ADOPT',
+    ],
+    [
+      'fastify/fastify',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/fastify',
+      'TRIAL',
+    ],
+    [
+      'immerjs/immer',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/immer',
+      'TRIAL',
+    ],
+    [
+      'immutable-js/immutable-js',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/immutable-js',
+      'TRIAL',
+    ],
+    [
+      'single-spa/single-spa',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/single-spa',
+      'TRIAL',
+    ],
+    [
+      'Microsoft/TypeScript',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/typescript',
+      'ADOPT',
+    ],
+    [
+      'BabylonJS/Babylon.js',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/babylon-js',
+      'ASSESS',
+    ],
+    [
+      'facebook/hermes',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/hermes',
+      'ASSESS',
+    ],
+    [
+      'gcanti/io-ts',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/io-ts',
+      'ASSESS',
+    ],
+    [
+      'Polymer/lit-element',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/litelement',
+      `ASSESS`,
+    ],
+    [
+      'mswjs/msw',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/mock-service-worker',
+      `ASSESS`,
+    ],
+    [
+      'vercel/swr',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/swr',
+      `ASSESS`,
+    ],
+    ['yarnpkg/yarn', 'https://www.thoughtworks.com/radar/tools/yarn', `TRIAL`],
+    [
+      'npm/cli',
+      'https://www.thoughtworks.com/radar/techniques/npm-for-all-the-things',
+      'TRIAL',
+    ],
+    [
+      'garris/backstopjs',
+      'https://www.thoughtworks.com/radar/tools/backstopjs',
+      'TRIAL',
+    ],
+    ['oblador/loki', 'https://www.thoughtworks.com/radar/tools/loki', 'TRIAL'],
+    [
+      'tailwindlabs/tailwindcss',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/tailwind-css',
+      'ASSESS',
+    ],
+    [
+      'DevExpress/testcafe',
+      'https://www.thoughtworks.com/radar/tools/testcafe',
+      'TRIAL',
+    ],
+    [
+      'cypress-io/cypress',
+      'https://www.thoughtworks.com/radar/tools/cypress',
+      'ADOPT',
+    ],
+    [
+      'puppeteer/puppeteer',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/puppeteer',
+      'TRIAL',
+    ],
+    [
+      'getsentry/sentry',
+      'https://www.thoughtworks.com/radar/tools/sentry',
+      'TRIAL',
+    ],
+    [
+      'styled-components/styled-components',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/styled-components',
+      'TRIAL',
+    ],
+    [
+      'snowpackjs/snowpack',
+      'https://www.thoughtworks.com/radar/tools/snowpack',
+      'ASSESS',
+    ],
+    [
+      'styleguidist/react-styleguidist',
+      'https://www.thoughtworks.com/radar/tools/react-styleguidist',
+      'ADOPT',
+    ],
+    ['pnpm/pnpm', 'https://www.thoughtworks.com/radar/tools/pnpm', 'ASSESS'],
+    [
+      'Microsoft/playwright',
+      'https://www.thoughtworks.com/radar/tools/playwright',
+      'ASSESS',
+    ],
+    [
+      'parcel-bundler/parcel',
+      'https://www.thoughtworks.com/radar/tools/parcel',
+      'ASSESS',
+    ],
+    [
+      'webpack/webpack',
+      'https://www.thoughtworks.com/radar/tools/webpack',
+      'TRIAL',
+    ],
+    [
+      'prettier/prettier',
+      'https://www.thoughtworks.com/radar/tools/prettier',
+      'TRIAL',
+    ],
+    [
+      'Netflix/pollyjs',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/polly-js',
+      'ASSESS',
+    ],
+    [
+      'nestjs/nest',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/nestjs',
+      'TRIAL',
+    ],
+    [
+      'bbyars/mountebank',
+      'https://www.thoughtworks.com/radar/tools/mountebank',
+      'ADOPT',
+    ],
+    [
+      'uber/manifold',
+      'https://www.thoughtworks.com/radar/tools/manifold',
+      'ASSESS',
+    ],
+    [
+      'mermaid-js/mermaid',
+      'https://www.thoughtworks.com/radar/tools/mermaid',
+      'TRIAL',
+    ],
+    [
+      'laconiajs/laconia',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/laconia',
+      'TRIAL',
+    ],
+    [
+      'facebook/jscodeshift',
+      'https://www.thoughtworks.com/radar/tools/jscodeshift',
+      'TRIAL',
+    ],
+    [
+      'gatsbyjs/gatsby',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/gatsby-js',
+      'ASSESS',
+    ],
+    [
+      'sideway/joi',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/joi',
+      'TRIAL',
+    ],
+    [
+      'jorgebucaran/hyperapp',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/hyperapp',
+      'ASSESS',
+    ],
+    [
+      'formium/formik',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/formik',
+      'TRIAL',
+    ],
+    [
+      'flowtype/flow-bin',
+      'https://www.thoughtworks.com/radar/tools/flow',
+      'ASSESS',
+    ],
+    [
+      '11ty/eleventy',
+      'https://www.thoughtworks.com/radar/tools/eleventy',
+      'ASSESS',
+    ],
+    [
+      'eslint/eslint',
+      'https://www.thoughtworks.com/radar/tools/eslint',
+      'ADOPT',
+    ],
+    [
+      'clinicjs/node-clinic-bubbleprof-examples',
+      'https://www.thoughtworks.com/radar/languages-and-frameworks/clinic-js-bubbleprof',
+      'ASSESS',
+    ],
+    [
+      'commitizen/cz-cli',
+      'https://www.thoughtworks.com/radar/tools/commitizen',
+      'ADOPT',
+    ],
+    ['avajs/ava', 'https://www.thoughtworks.com/radar/tools/ava', 'TRIAL'],
+  ];
+}
