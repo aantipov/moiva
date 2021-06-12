75eedd68c1fe7e6b39e621f8e9c714a6037f2a7d
Alexey Antipov <antipov.alexei@gmail.com>
2021-06-12 15:59:06 +0200

    add status badges (#362)
    * add status badges
    
    * cleanup

---
+++ src/components/StatusBadge.vue
@@ -0,0 +1,43 @@
+<template>
+  <span
+    class="inline-block px-2 py-1 mr-1 text-xs font-semibold text-white uppercase rounded-full last:mr-0"
+    :class="[value.toLowerCase()]"
+  >
+    {{ value }}
+  </span>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+import { StatusT } from '@/getLibrary';
+
+export default defineComponent({
+  name: 'StatusBadge',
+
+  props: {
+    value: {
+      type: String as PropType<StatusT>,
+      required: true,
+    },
+  },
+
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style lang="postcss" scoped>
+.active {
+  background-color: #42bd14;
+}
+.inactive {
+  @apply bg-gray-500;
+}
+.archived {
+  background-color: #ce5740;
+}
+.legacy {
+  background-color: #e77131;
+}
+</style>
