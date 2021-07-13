d2b7ef47a93a0a0c117b16858c704cc8bb1f375c
Alexey Antipov <antipov.alexei@gmail.com>
2021-07-13 19:02:29 +0300

    add catalog tests

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+// jest.config.js
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  verbose: true,
+  preset: 'vite-jest',
+};
+
+module.exports = config;
--- src/data/index.spec.ts
+++ src/data/index.spec.ts
@@ -1,25 +1,68 @@
-const techRadarItems = require('./techradar.json'); // eslint-disable-line
-const googleTrendsItems = require('./google-trends.json'); // eslint-disable-line
-const readingsItems = require('./readings.json'); // eslint-disable-line
-const stateofjsItems = require('./stateofjscss.json'); // eslint-disable-line
-const libs = require('./libraries.json'); // eslint-disable-line
+import techRadarItems from './techradar.json';
+import googleTrendsItems from './google-trends.json';
+import readingsItems from './readings.json';
+import stateofjsItems from './stateofjscss.json';
+import { catalogLibraries, CatalogLibraryT } from './index';
+
 // @ts-ignore
-const libsReposIds = libs.map((lib) => lib.repoId);
+const libsReposIds = catalogLibraries.map((lib) => lib.repoId);
 
 describe('ThoughtWorks Radar', () => {
   it('repos names match Catalog', () => {
-    // @ts-ignore
+    // List of TechRadar Items exceptions that are not part of Catalog (no npm package)
+    const exceptions = [
+      'laravel/laravel',
+      'getsentry/sentry',
+      'pnpm/pnpm',
+      'uber/manifold',
+      'backstage/backstage',
+      'materializeinc/materialize',
+      'apache/incubator-pinot',
+      'linkedin/datahub',
+      'juicedata/juicefs',
+      'opstrace/opstrace',
+      'vectorizedio/redpanda',
+      'facebook/flipper',
+      'k6io/k6',
+      'mlflow/mlflow',
+      'google/or-tools',
+      'toniblyx/prowler',
+      'microsoft/pyright',
+      'getredash/redash',
+      'gruntwork-io/terratest',
+      'oracle/graal',
+      'hashicorp/boundary',
+      'imgcook/imgcook',
+      'clojure/clojurescript',
+      'longhorn/longhorn',
+      'yelp/detect-secrets',
+      'zalando/zally',
+      'square/leakcanary',
+      'awslabs/aws-data-wrangler',
+      'tiangolo/fastapi',
+      'streamlit/streamlit',
+      'bunit-dev/bunit',
+      'dagster-io/dagster',
+      'lvgl/lvgl',
+      'online-ml/river',
+    ];
     techRadarItems.forEach((item) => {
-      expect(libsReposIds).toContain(item[0]);
+      if (!exceptions.includes(item[0] as string)) {
+        expect(libsReposIds).toContain(item[0]);
+      }
     });
   });
 });
 
 describe('Google Trends', () => {
   it('repos names match Catalog', () => {
-    // @ts-ignore
+    // List of Google Trends Items exceptions that are not part of Catalog (no npm package)
+    const exceptions = ['laravel/laravel', 'django/django'];
+
     googleTrendsItems.forEach((item) => {
-      expect(libsReposIds).toContain(item[0]);
+      if (!exceptions.includes(item[0] as string)) {
+        expect(libsReposIds).toContain(item[0]);
+      }
     });
   });
 });
@@ -38,75 +81,34 @@ describe('Readings', () => {
 
 describe('StateOfJS', () => {
   it('repos names match Catalog', () => {
-    // @ts-ignore
+    // List of StateOfJS Items exceptions that are not part of Catalog (no npm package).
+    const exceptions = [
+      'clojure/clojurescript',
+      'elm/compiler',
+      'graphql/graphql-spec',
+      'meteor/meteor',
+      'purescript/purescript',
+      'reasonml/reason',
+      'sass/node-sass',
+    ];
     stateofjsItems.forEach((item) => {
-      expect(libsReposIds).toContain(item.repoId);
+      if (!exceptions.includes(item.repoId)) {
+        expect(libsReposIds).toContain(item.repoId);
+      }
     });
   });
 });
 
-describe('Catalog libraries', () => {
-  it.todo('Contain No duplicates');
-  // function getDuplicates(categories) {
-  //   // TODO: improve this logic
-  //   const libs = categories.map(({ libs }) => libs).flat();
-  //   const libsNpmNames = libs.map(({ npm }) => npm).filter((npm) => !!npm);
-  //   const libsReposIds = libs
-  //     .filter(({ npm }) => !npm)
-  //     .map(({ repoId }) => repoId);
-  //   const libsAliases = libs.map(({ alias }) => alias);
-  //   const duplicates = [];
-
-  //   duplicates.push(
-  //     ...getDuplicatesGeneric(libsNpmNames),
-  //     ...getDuplicatesGeneric(libsReposIds),
-  //     ...getDuplicatesGeneric(libsAliases)
-  //   );
-
-  //   return duplicates;
-  // }
-
-  // function getDuplicatesGeneric(items) {
-  //   const duplicates = [];
-
-  //   if (new Set(items).size < items.length) {
-  //     const itemsMap = {};
-
-  //     items.forEach((item) => {
-  //       if (itemsMap[item]) {
-  //         duplicates.push(item);
-  //         return;
-  //       }
-  //       itemsMap[item] = true;
-  //     });
-  //   }
-
-  //   return duplicates;
-  // }
-  // const duplicates = getDuplicates(categories);
-  // if (duplicates.length) {
-  //   console.error(`Duplicate libraries names: ${duplicates.join('; ')}`);
-  //   return;
-  // }
+describe.skip('Catalog libraries', () => {
+  it('Contain No duplicates', () => {
+    const duplicates = getDuplicates(catalogLibraries);
+    if (duplicates.length) {
+      console.error(`Duplicate libraries names: ${duplicates.join('; ')}`);
+      return;
+    }
+  });
 
   it.todo('Use correct frameworks');
-  // function getWrongFrameworks(categories) {
-  //   const libs = categories.map(({ libs }) => libs).flat();
-  //   const frameworks = libs.map(({ framework }) => framework);
-  //   const allowedFrameworks = [
-  //     null,
-  //     'react',
-  //     'vue',
-  //     'svelte',
-  //     'ember',
-  //     'angular',
-  //     'angularjs',
-  //   ];
-
-  //   return frameworks.filter(
-  //     (framework) => !allowedFrameworks.includes(framework)
-  //   );
-  // }
   // const wrongFrameworks = getWrongFrameworks(categories);
   // if (wrongFrameworks.length) {
   //   console.error(`Wrong frameworks found: ${wrongFrameworks.join('; ')}`);
@@ -115,3 +117,58 @@ describe('Catalog libraries', () => {
 
   it.todo('all repos lowercase - Should or Not?');
 });
+
+function getDuplicates(libs: CatalogLibraryT[]) {
+  const libsNpmNames = libs
+    .map(({ npm }) => npm)
+    .filter((npm) => !!npm) as string[];
+  const libsReposIds = libs
+    .filter(({ npm }) => !npm)
+    .map(({ repoId }) => repoId);
+  const libsAliases = libs.map(({ alias }) => alias);
+  const duplicates = [];
+
+  duplicates.push(
+    ...getDuplicatesGeneric(libsNpmNames),
+    ...getDuplicatesGeneric(libsReposIds),
+    ...getDuplicatesGeneric(libsAliases)
+  );
+
+  return duplicates;
+}
+
+function getDuplicatesGeneric(items: string[]) {
+  const duplicates: string[] = [];
+
+  if (new Set(items).size < items.length) {
+    const itemsMap = {} as Record<string, boolean>;
+
+    items.forEach((item) => {
+      if (itemsMap[item]) {
+        duplicates.push(item);
+        return;
+      }
+      itemsMap[item] = true;
+    });
+  }
+
+  return duplicates;
+}
+
+// function getWrongFrameworks(categories) {
+//   const libs = categories.map(({ libs }) => libs).flat();
+//   const frameworks = libs.map(({ framework }) => framework);
+//   const allowedFrameworks = [
+//     null,
+//     'react',
+//     'vue',
+//     'svelte',
+//     'ember',
+//     'angular',
+//     'angularjs',
+//   ];
+
+//   return frameworks.filter(
+//     (framework) => !allowedFrameworks.includes(framework)
+//   );
+// }
