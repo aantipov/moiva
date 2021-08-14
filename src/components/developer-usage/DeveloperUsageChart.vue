78e258de7c4b489cd26b76170ee203d672e6a413
Alexey Antipov <antipov.alexei@gmail.com>
2021-08-14 08:51:18 +0200

    refactor devusage chart

--- src/components/developer-usage/DeveloperUsageChart.vue
+++ src/components/developer-usage/DeveloperUsageChart.vue
@@ -20,8 +20,8 @@
   </m-chart>
 </template>
 
-<script lang="ts">
-import { defineComponent, computed, watchEffect } from 'vue';
+<script setup lang="ts">
+import { computed, watchEffect } from 'vue';
 import { ChartConfiguration } from 'chart.js';
 import { StateOfJSItemT } from '@/data/index';
 import { LibraryReadonlyT } from '@/libraryApis';
@@ -31,85 +31,76 @@ import { chartsVisibility } from '@/store/chartsVisibility';
 interface FilteredLibT extends LibraryReadonlyT {
   devUsage: StateOfJSItemT;
 }
+const filteredLibsRef = computed(
+  () => librariesRR.filter((lib) => !!lib.devUsage) as FilteredLibT[]
+);
 
-export default defineComponent({
-  name: 'DevelopersUsageChart',
-
-  setup() {
-    const filteredLibsRef = computed(
-      () => librariesRR.filter((lib) => !!lib.devUsage) as FilteredLibT[]
+watchEffect(() => {
+  chartsVisibility.developerUsage =
+    filteredLibsRef.value.length > 0 &&
+    !(
+      filteredLibsRef.value.length === 1 &&
+      filteredLibsRef.value[0].devUsage.usage.length === 1
     );
+});
 
-    watchEffect(() => {
-      chartsVisibility.developerUsage =
-        filteredLibsRef.value.length > 0 &&
-        !(
-          filteredLibsRef.value.length === 1 &&
-          filteredLibsRef.value[0].devUsage.usage.length === 1
-        );
-    });
-
-    const chartConfig = computed<ChartConfiguration>(() => ({
-      type: 'line',
-      data: {
-        datasets: filteredLibsRef.value.map((lib) => ({
-          label: lib.devUsage.name,
-          data: lib.devUsage.usage.map((usageItem) => ({
-            x: usageItem.year.toString() as unknown as number,
-            y: usageItem.value,
-          })),
-          spanGaps: true,
-          backgroundColor: lib.color,
-          borderColor: lib.color,
-        })),
-      },
-
-      options: {
-        normalized: true,
-        scales: {
-          x: {
-            type: 'time',
-            time: { unit: 'year' },
-          },
-          y: {
-            ticks: {
-              beginAtZero: false,
-              precision: 0,
-              callback: (val): string => val + '%',
-            },
-          },
-        },
-        plugins: {
-          tooltip: {
-            callbacks: {
-              label: (context): string => {
-                return ` ${context.dataset.label}: ${context.formattedValue}%`;
-              },
-            },
-          },
-        },
-      },
-    }));
-
-    return {
-      isDisplayed: computed(() => chartsVisibility.developerUsage),
-      isLoading,
-      chartConfig,
-      libsNames: computed(() =>
-        filteredLibsRef.value.map((lib) => lib.devUsage.name)
-      ),
-      ariaLabel: computed(() => {
-        const str = filteredLibsRef.value
-          .map(
-            (lib) =>
-              `${lib.alias} is used by ${
-                lib.devUsage.usage.slice(-1)[0].value
-              }% of developers.`
-          )
-          .join(' ');
-        return `Developer Usage statistics according to StateOfJS survey. ${str}`;
-      }),
-    };
+const chartConfig = computed<ChartConfiguration>(() => ({
+  type: 'line',
+  data: {
+    datasets: filteredLibsRef.value.map((lib) => ({
+      label: lib.devUsage.name,
+      data: lib.devUsage.usage.map((usageItem) => ({
+        x: usageItem.year.toString() as unknown as number,
+        y: usageItem.value,
+      })),
+      spanGaps: true,
+      backgroundColor: lib.color,
+      borderColor: lib.color,
+    })),
   },
+
+  options: {
+    normalized: true,
+    scales: {
+      x: {
+        type: 'time',
+        time: { unit: 'year' },
+      },
+      y: {
+        ticks: {
+          beginAtZero: false,
+          precision: 0,
+          callback: (val): string => val + '%',
+        },
+      },
+    },
+    plugins: {
+      tooltip: {
+        callbacks: {
+          label: (context): string => {
+            return ` ${context.dataset.label}: ${context.formattedValue}%`;
+          },
+        },
+      },
+    },
+  },
+}));
+
+const isDisplayed = computed(() => chartsVisibility.developerUsage);
+
+const libsNames = computed(() =>
+  filteredLibsRef.value.map((lib) => lib.devUsage.name)
+);
+
+const ariaLabel = computed(() => {
+  const str = filteredLibsRef.value
+    .map(
+      (lib) =>
+        `${lib.alias} is used by ${
+          lib.devUsage.usage.slice(-1)[0].value
+        }% of developers.`
+    )
+    .join(' ');
+  return `Developer Usage statistics according to StateOfJS survey. ${str}`;
 });
 </script>
