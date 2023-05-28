dc8fe0f207182e62193a981d221d2f69b33a47c3
Alexey Antipov <antipov.alexei@gmail.com>
2023-05-28 17:41:26 +0200

    use astro

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { createApp, DirectiveBinding } from 'vue';
+import { DirectiveBinding } from 'vue';
+import type { App } from 'vue';
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/dist/backdrop.css';
@@ -10,9 +11,8 @@ import * as Sentry from '@sentry/vue';
 import { Integrations } from '@sentry/tracing';
 import '../chartjs.config';
 import { VueQueryPlugin } from 'vue-query';
-import App from '@/App.vue';
-import CatalogApp from './CatalogApp.vue';
-import AboutApp from './AboutApp.vue';
+// import CatalogApp from './CatalogApp.vue';
+// import AboutApp from './AboutApp.vue';
 import Close from '@/icons/CloseIcon.vue';
 import StarIcon from '@/icons/StarIcon.vue';
 import HomeIcon from '@/icons/HomeIcon.vue';
@@ -46,99 +46,100 @@ import Loader from '@/components/LoadingView.vue';
 import LoaderNew from '@/components/LoaderNew.vue';
 import './assets/tailwind.css';
 
-const app = createApp(App);
+export default (app: App) => {
+  if (process.env.NODE_ENV !== 'development') {
+    Sentry.init({
+      app,
+      dsn: 'https://185bd9a836b146318babbd956881e8a0@o477177.ingest.sentry.io/5517696',
+      logErrors: true,
+      integrations: [
+        new Integrations.BrowserTracing({
+          tracingOrigins: ['localhost', 'moiva.io', /^\//],
+        }),
+      ],
+      // process.env.NODE_ENV is being replaced by the value during build
+      environment: process.env.NODE_ENV,
+      // We recommend adjusting this value in production, or using tracesSampler
+      // for finer control
+      tracesSampleRate: 1.0,
+    });
+  }
 
-if (process.env.NODE_ENV !== 'development') {
-  Sentry.init({
-    app,
-    dsn: 'https://185bd9a836b146318babbd956881e8a0@o477177.ingest.sentry.io/5517696',
-    logErrors: true,
-    integrations: [
-      new Integrations.BrowserTracing({
-        tracingOrigins: ['localhost', 'moiva.io', /^\//],
-      }),
-    ],
-    // process.env.NODE_ENV is being replaced by the value during build
-    environment: process.env.NODE_ENV,
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
+  app.use(VueQueryPlugin);
+
+  // Table metrics icons
+  app
+    .component('MRepoIcon', GithubIcon)
+    .component('MStatusIcon', HeartBeatIcon)
+    .component('MStarsIcon', StarIcon)
+    .component('MHomeIcon', HomeIcon)
+    .component('MDownloadsIcon', DownloadIcon)
+    .component('MSearchIcon', SearchIcon)
+    .component('MDevusageIcon', UserGroupIcon)
+    .component('MTradarIcon', TWIcon)
+    .component('MReleasesIcon', TagIcon)
+    .component('MCommitsIcon', CommitsIcon)
+    .component('MContributorsIcon', WorkerIcon)
+    .component('MDependenciesIcon', DependencyIcon)
+    .component('MLicenseIcon', DocumentIcon)
+    .component('MAgeIcon', OldIcon)
+    .component('MVulnerabilityIcon', BugIcon)
+    .component('MSecurityIcon', ShieldIcon)
+    .component('MBundlesizeIcon', CubeIcon)
+    .component('MTsIcon', TSIcon)
+    .component('MSortIcon', SortIcon);
+
+  app
+    .component('MClose', Close)
+    .component('MTag', Tag)
+    .component('MDotsIcon', DotsIcon)
+    .component('MStarIcon', StarIcon)
+    .component('MNpmIcon', NpmIcon)
+    .component('MChartIcon', ChartIcon)
+    .component('MGithubIcon', GithubIcon)
+    .component('MDownloadIcon', DownloadIcon)
+    .component('MChartInfo', ChartInfo)
+    .component('MLoaderTailSpin', LoaderTailSpin)
+    .component('MLoader', Loader)
+    .component('MLoaderNew', LoaderNew)
+    .component('MExtLink', ExternalLink)
+    .component('MChart', ChartPresentation)
+    .component('MChartMenu', ChartMenu);
+
+  app.directive('tooltip', {
+    mounted(el, binding) {
+      if (binding.value) {
+        initTooltip(el, binding);
+      }
+    },
+    updated(el, binding) {
+      if (el._tippy) {
+        el._tippy.setContent(binding.value);
+      } else {
+        initTooltip(el, binding);
+      }
+    },
   });
-}
 
-app.use(VueQueryPlugin);
+  function initTooltip(el: unknown, binding: DirectiveBinding<any>) {
+    tippy(el as HTMLElement, {
+      content: binding.value,
+      delay: 150,
+      interactive: 'ni' in binding.modifiers ? !binding.modifiers.ni : true, // non-interactive
+      allowHTML: binding.modifiers.html,
+      appendTo: document.body,
+    });
+  }
 
-// Table metrics icons
-app
-  .component('MRepoIcon', GithubIcon)
-  .component('MStatusIcon', HeartBeatIcon)
-  .component('MStarsIcon', StarIcon)
-  .component('MHomeIcon', HomeIcon)
-  .component('MDownloadsIcon', DownloadIcon)
-  .component('MSearchIcon', SearchIcon)
-  .component('MDevusageIcon', UserGroupIcon)
-  .component('MTradarIcon', TWIcon)
-  .component('MReleasesIcon', TagIcon)
-  .component('MCommitsIcon', CommitsIcon)
-  .component('MContributorsIcon', WorkerIcon)
-  .component('MDependenciesIcon', DependencyIcon)
-  .component('MLicenseIcon', DocumentIcon)
-  .component('MAgeIcon', OldIcon)
-  .component('MVulnerabilityIcon', BugIcon)
-  .component('MSecurityIcon', ShieldIcon)
-  .component('MBundlesizeIcon', CubeIcon)
-  .component('MTsIcon', TSIcon)
-  .component('MSortIcon', SortIcon);
+  // TODO: remove this: appCatalog and appAbout are not used anymore
+  // const appCatalog = new URL(window.location.href).searchParams.get('appcatalog');
+  // const appAbout = new URL(window.location.href).searchParams.get('appabout');
 
-app
-  .component('MClose', Close)
-  .component('MTag', Tag)
-  .component('MDotsIcon', DotsIcon)
-  .component('MStarIcon', StarIcon)
-  .component('MNpmIcon', NpmIcon)
-  .component('MChartIcon', ChartIcon)
-  .component('MGithubIcon', GithubIcon)
-  .component('MDownloadIcon', DownloadIcon)
-  .component('MChartInfo', ChartInfo)
-  .component('MLoaderTailSpin', LoaderTailSpin)
-  .component('MLoader', Loader)
-  .component('MLoaderNew', LoaderNew)
-  .component('MExtLink', ExternalLink)
-  .component('MChart', ChartPresentation)
-  .component('MChartMenu', ChartMenu);
-
-app.directive('tooltip', {
-  mounted(el, binding) {
-    if (binding.value) {
-      initTooltip(el, binding);
-    }
-  },
-  updated(el, binding) {
-    if (el._tippy) {
-      el._tippy.setContent(binding.value);
-    } else {
-      initTooltip(el, binding);
-    }
-  },
-});
-
-function initTooltip(el: unknown, binding: DirectiveBinding<any>) {
-  tippy(el as HTMLElement, {
-    content: binding.value,
-    delay: 150,
-    interactive: 'ni' in binding.modifiers ? !binding.modifiers.ni : true, // non-interactive
-    allowHTML: binding.modifiers.html,
-    appendTo: document.body,
-  });
-}
-
-const appCatalog = new URL(window.location.href).searchParams.get('appcatalog');
-const appAbout = new URL(window.location.href).searchParams.get('appabout');
-
-if (appCatalog) {
-  createApp(CatalogApp).mount('#app');
-} else if (appAbout) {
-  createApp(AboutApp).mount('#app');
-} else {
-  app.mount('#app');
-}
+  // if (appCatalog) {
+  //   createApp(CatalogApp).mount('#app');
+  // } else if (appAbout) {
+  //   createApp(AboutApp).mount('#app');
+  // } else {
+  //   app.mount('#app');
+  // }
+};
