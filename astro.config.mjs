47076b77b0d6581b53ba501cc7a34794b696be0e
Alexey Antipov <antipov.alexei@gmail.com>
2023-05-30 22:09:12 +0200

    Deploy to cloudflare (#569)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   outDir: './dist',
   site: 'https://moiva.io/',
   integrations: [vue({ appEntrypoint: '/src/main' }), tailwind()],
-  experimental: {
-    inlineStylesheets: 'auto',
-  },
+  experimental: { inlineStylesheets: 'auto' },
   // vite: {
+  // build: { minify: false },
   //   resolve: { alias: { '@': '/src' } },
   // },
+  output: 'server',
+  adapter: cloudflare({ mode: 'directory' }),
 });
