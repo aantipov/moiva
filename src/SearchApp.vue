c1c20289c734436ad8a6c3f8c91dd9687de844d6
Alexey Antipov <antipov.alexei@gmail.com>
2023-06-01 17:22:39 +0200

    extract search into astra component (#571)

---
+++ src/SearchApp.vue
@@ -0,0 +1,16 @@
+<template>
+  <Search class="mx-auto w-full lg:w-9/12 xl:w-2/4" @select="select" />
+</template>
+
+<script setup lang="ts">
+import Search from '@/components/search/Search.vue';
+import { constructHref } from '@/utils';
+
+function select(id: string, isNpm: boolean) {
+  if (isNpm) {
+    window.location.assign(constructHref([id], []));
+  } else {
+    window.location.assign(constructHref([], [id]));
+  }
+}
+</script>
