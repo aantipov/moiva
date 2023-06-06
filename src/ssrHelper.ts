91db11693af3cdcd5ef3b7d9412cff5a8319d0ee
Alexey Antipov <antipov.alexei@gmail.com>
2023-06-06 16:32:00 +0200

    seo for multiple packages

--- src/ssrHelper.ts
+++ src/ssrHelper.ts
@@ -12,17 +12,27 @@ export function isSEOPage(npmPackages: string[], reposIds: string[]): boolean {
 }
 
 export function getSEOTitle(npmPackages: string[]): string {
-  if (npmPackages.length === 1) {
-    return `"${npmPackages[0]}" NPM Package: Comprehensive Analysis, Alternatives, and Usage Statistics | Moiva.io`;
+  if (npmPackages.length === 0) {
+    return 'NPM Packages Comparison | Performance, Security & Trends';
+  } else if (npmPackages.length === 1) {
+    return `${npmPackages[0]} NPM Package: Comprehensive Analysis, Alternatives, and Usage Statistics | Moiva.io`;
+  } else {
+    return `${npmPackages.join(
+      ' vs '
+    )}: Detailed NPM Packages Comparison | Performance, Security & Trends`;
   }
-  // TODO: add support for multiple packages
-  return 'Moiva.io - Discover and Compare GitHub and NPM packages';
 }
 
 export function getSEODescription(npmPackages: string[]): string {
-  if (npmPackages.length === 1) {
-    return `Explore our in-depth analysis of the "${npmPackages[0]}" NPM package. Discover its distinct features, use-cases, alternatives, and comprehensive comparison metrics. Explore detailed statistics like NPM downloads, Github star trends, Google search interest, and more.`;
+  if (npmPackages.length === 0) {
+    return `Compare NPM packages and make informed decisions. Explore detailed statistics like NPM downloads, Github star trends, Google search interest, and more. Empower your tech stack decisions with data-driven insights.`;
+  } else if (npmPackages.length === 1) {
+    return `Explore our in-depth analysis of the ${npmPackages[0]} NPM package. Discover its distinct features, use-cases, alternatives, and comprehensive comparison metrics. Explore detailed statistics like NPM downloads, Github star trends, Google search interest, and more.`;
+  } else {
+    return `In-depth comparison of ${npmPackages
+      .slice(0, -1)
+      .join(', ')} and ${npmPackages.at(
+      -1
+    )} npm packages, offering insights on bundle size, download trends, Google search interest, GitHub Stars, license, contributors activity, security scores, tech radar blips, and more. Empower your tech stack decisions with data-driven insights.`;
   }
-  // TODO: add support for multiple packages
-  return 'A complete side-by-side Comparison of NPM packages. Stats and Historical Trends - Bundle size, Downloads, GitHub Stars, License, Contributors, Releases, Commits, Developer usage, Google Trends, Vulnerabilities, Dependencies, Issues, Age and more';
 }
