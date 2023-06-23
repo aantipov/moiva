1241c77b9b70b9be3b07f331ee5efeea310f3c48
Alexey Antipov <antipov.alexei@gmail.com>
2023-06-23 09:19:12 +0200

    add snyk security score

--- src/AiContentApp/NpmContent.vue
+++ src/AiContentApp/NpmContent.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col antialiased">
     <h2 v-if="showTitle" class="self-center font-mono">{{ pkg.name }}</h2>
 
-    <p class="flex">
+    <p class="flex items-center">
       <a
         v-if="showHomeLink"
         v-tooltip.html="'Home Page'"
@@ -28,10 +28,18 @@
         :href="`https://github.com/${pkg.repoId}`"
         target="_blank"
         rel="noopener"
-        class="text-black"
+        class="mr-1 text-black"
       >
         <GithubIcon />
       </a>
+
+      <object
+        :id="'badge-' + props.pkg.name"
+        v-tooltip.html="snykTooltip"
+        :data="snykUrl"
+        type="image/svg+xml"
+        style="max-width: 131px; max-height: 20px"
+      ></object>
     </p>
 
     <p v-for="(p, i) in description" :key="i" class="self-center pb-2">
@@ -76,4 +84,13 @@ const showHomeLink = computed(
     props.pkg.homepage.startsWith('https://') &&
     !props.pkg.homepage.includes('github.com')
 );
+
+const snykUrl = computed(
+  () =>
+    `https://snyk-widget.herokuapp.com/badge/npm/${encodeURIComponent(
+      props.pkg.name
+    )}/badge.svg`
+);
+const snykTooltip =
+  '<p>Security score of the Npm package.</p> <p><a href="https://snyk.io/" target="_blank">Snyk.io</a> calculates it based on the number of vulnerabilities and their severity.</p> "A" - no vulnerabilities, "F" - the least secure level.';
 </script>
