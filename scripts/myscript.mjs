4eafd63d8d117cca766b69fabfd9e3cf95ed4962
Alexey Antipov <antipov.alexei@gmail.com>
2023-06-27 10:49:24 +0200

    update libs (#621)

--- scripts/myscript.mjs
+++ scripts/myscript.mjs
@@ -5,15 +5,30 @@
  * px zx scripts/myscript.mjs
  */
 
-const libsAll = require('.../src/data/libraries.json');
+const bunchNum = 14;
+const bunshSize = 50;
+const libsAll = require('../src/data/libraries.json');
 const libsFiltered = libsAll.filter(
   (lib) => !!lib.npm && !!lib.isNpmCoreArtifact
 );
-const libs = libsFiltered.slice(0, 50).map((lib) => lib.npm);
+console.log('COUNT', libsFiltered.length);
+const libs = libsFiltered
+  .slice(bunchNum * bunshSize, (bunchNum + 1) * bunshSize)
+  .map((lib) => lib.npm);
+console.log('START-END', bunchNum * bunshSize, (bunchNum + 1) * bunshSize);
 const libsPromises = libs.map((lib) =>
   fetch(`https://moiva.io/npm-info/${encodeURIComponent(lib)}`)
+    .then((res) => res.json())
+    .then((pkg) => {
+      console.log('SUCCESS', lib, pkg.npm.name, pkg.npm.publishedAt);
+    })
+    .catch((err) => console.error('ERROR', lib, err))
 );
-const libsResponses = await Promise.all(libsPromises);
-const libsJsons = await Promise.all(libsResponses.map((res) => res.json()));
-const libsJsonsNoAi = libsJsons.filter((lib) => !lib.ai).map((lib) => lib.name);
-console.log('Libs without AI data \n', libsJsonsNoAi);
+try {
+  await Promise.all(libsPromises);
+} catch (error) {
+  console.error('final error', error);
+}
+// const libsJsonsNoAi = libsJsons.filter((lib) => !lib.ai).map((lib) => lib.name);
+// console.log('Libs without AI data \n', libsJsonsNoAi);
+console.log('FINISH');
