2953d5f564608301332777c7098327e1ac5956b0
Alexey Antipov <antipov.alexei@gmail.com>
2023-07-26 10:05:39 +0200

    techradar: use npm name vs repo as library id (#639)

--- src/data/index.spec.ts
+++ src/data/index.spec.ts
@@ -6,52 +6,14 @@ import stateofjsItems from './stateofjscss.json';
 import { allCatalogLibraries as catalogLibraries } from './index';
 
 // @ts-ignore
+const libsNpmNames = catalogLibraries.map((lib) => lib.npm);
 const libsReposIds = catalogLibraries.map((lib) => lib.repoId);
 
 describe('ThoughtWorks Radar', () => {
-  it('repos names match Catalog', () => {
+  it('npm names match Catalog', () => {
     // List of TechRadar Items exceptions that are not part of Catalog (no npm package)
-    const exceptionsWithoutNpm = [
-      'laravel/laravel',
-      'getsentry/sentry',
-      'pnpm/pnpm',
-      'uber/manifold',
-      'backstage/backstage',
-      'materializeinc/materialize',
-      'apache/incubator-pinot',
-      'linkedin/datahub',
-      'juicedata/juicefs',
-      'opstrace/opstrace',
-      'vectorizedio/redpanda',
-      'facebook/flipper',
-      'k6io/k6',
-      'mlflow/mlflow',
-      'google/or-tools',
-      'toniblyx/prowler',
-      'microsoft/pyright',
-      'getredash/redash',
-      'gruntwork-io/terratest',
-      'oracle/graal',
-      'hashicorp/boundary',
-      'imgcook/imgcook',
-      'clojure/clojurescript',
-      'longhorn/longhorn',
-      'yelp/detect-secrets',
-      'zalando/zally',
-      'square/leakcanary',
-      'awslabs/aws-data-wrangler',
-      'tiangolo/fastapi',
-      'streamlit/streamlit',
-      'bunit-dev/bunit',
-      'dagster-io/dagster',
-      'lvgl/lvgl',
-      'online-ml/river',
-    ];
-
     techRadarItems.forEach((item) => {
-      if (!exceptionsWithoutNpm.includes(item[0] as string)) {
-        expect(libsReposIds).toContain(item[0]);
-      }
+        expect(libsNpmNames).toContain(item[0]);
     });
   });
 });
