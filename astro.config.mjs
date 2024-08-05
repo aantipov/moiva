7c06ad873f8b14b0598fef68f1035f4ec4fe9bdf
Alexey Antipov <antipov.alexei@gmail.com>
2024-08-05 13:00:52 +0200

    update astro, openai and wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,11 @@ export default defineConfig({
   site: 'https://moiva.io/',
   integrations: [vue({ appEntrypoint: '/src/main' }), tailwind()],
   build: { inlineStylesheets: 'auto' },
-  // vite: {
-  // build: { minify: false },
-  //   resolve: { alias: { '@': '/src' } },
-  // },
+  vite: {
+    // build: { minify: false },
+    //   resolve: { alias: { '@': '/src' } },
+    ssr: { noExternal: ['path-to-regexp'] },
+  },
   output: 'hybrid',
   adapter: cloudflare({ mode: 'directory' }),
 });
